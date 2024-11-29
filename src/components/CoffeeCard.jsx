88f4964c5718130107026fe510ea7a200965b760
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-11-29 15:05:15 +0600

    Update Successfull.

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
@@ -47,7 +48,10 @@ const CoffeeCard = ({ coffee }) => {
         <div className="card-actions justify-end">
           <div className="btn-group btn-group-vertical flex flex-col space-y-4">
             <button className="btn">View</button>
+            <Link to={`updateCoffee/${_id}`}>
             <button className="btn">Edit</button>
+            </Link>
+            
             <button onClick={() => handleDelete(_id)} className="btn">
               X
             </button>
